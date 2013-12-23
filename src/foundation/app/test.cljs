5d2c325da6f0dfb3a55e06967540423cfa8be649
Adrian Medina <adrian.medina@mail.yu.edu>
2013-12-23 12:03:00 -0500

    generative testing functions

--- src/foundation/app/test.cljs
+++ src/foundation/app/test.cljs
@@ -5,3 +5,15 @@
 (defn rand-str
   [size alphabet]
   (apply str (repeatedly size #(rand-nth alphabet))))
+
+(def rand-sym #(symbol (rand-str %1 %2)))
+
+(def rand-key #(keyword (rand-str %1 %2)))
+
+(defn rand-vec
+  [& generators]
+  (into [] (map apply generators)))
+
+(defn rand-map
+  [size kgen vgen]
+  (into {} (repeatedly size #(rand-vec kgen vgen))))
